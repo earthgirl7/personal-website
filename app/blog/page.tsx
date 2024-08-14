b87435dcbdc254d5c83438913a7f997433d45af2
anosharahim <anosharahim03@gmail.com>
2024-08-14 10:59:21 -0700

    style blog entry page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -18,11 +18,11 @@ const BlogList = () => {
     <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex-grow py-16 flex justify-center">
-        <div className="w-2/5">
+        <div className="w-1/2">
           <div className="text-4xl flex justify-center font-sans">
             The Inside of My Brain
           </div>
-          <hr className="border-t border-black my-4" />
+          <hr className="border-t border-current border-t-[0.5px] my-4" />
 
           {blogData.map((blog) => (
             <Link
@@ -32,10 +32,18 @@ const BlogList = () => {
               key={blog.id}
             >
               <div className="flex items-center py-4 group">
+                <div
+                  className={`w-32 mr-10 group-hover:text-hover-color ${
                     hashtagColors[blog.hashtag] || "text-gray-500"
+                  }`}
+                >
+                  {blog.hashtag}
+                </div>
+                <div className="flex-1 flex justify-between items-center font-light">
+                  <div className=" group-hover:text-hover-color">
                     {blog.title}
                   </div>
-                  <div className="whitespace-nowrap group-hover:text-gray-500">
+                  <div className="whitespace-nowrap group-hover:text-hover-color font-light">
                     {blog.date}
                   </div>
                 </div>

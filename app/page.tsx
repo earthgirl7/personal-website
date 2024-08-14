8c2ac04bf6d0294b017bd557b35d4eab3ddc02b1
anosharahim <anosharahim03@gmail.com>
2024-08-14 11:45:48 -0700

    add dynamic word change

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,54 @@
+"use client";
+import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 export default function Home() {
+  const words = [
+    "AI engineer",
+    "lifelong learner",
+    "traveler",
+    "creative",
+    "athlete",
+  ];
+  const [currentWord, setCurrentWord] = useState(words[0]);
+  const [isChanging, setIsChanging] = useState(false);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setIsChanging(true);
+      setTimeout(() => {
+        setCurrentWord((prevWord) => {
+          const currentIndex = words.indexOf(prevWord);
+          const nextIndex = (currentIndex + 1) % words.length;
+          return words[nextIndex];
+        });
+        setIsChanging(false);
+      }, 500); // Half of the interval for smooth transition
+    }, 2000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex-grow container mx-auto py-6 flex items-center justify-center">
         <div className="text-center">
-          <p className="text-xl font-light font-sans">Hi, my name is</p>
-          <h1 className="text-4xl font-sans font-semi-light  ">ANOSHA RAHIM</h1>
-          <p className="text-xl font-light  font-sans">
-            and I am an AI engineer
+          {/* <p className="text-xl font-light font-sans mb-5">Hi, my name is</p> */}
+          <h1 className="text-4xl font-sans font-semi-light mb-8">
+            ANOSHA RAHIM
+          </h1>
+
+          <p className="text-2xl font-light font-sans flex justify-center items-center mb-8">
+            <span className="word-container">
+              <span className={`word-change ${isChanging ? "changing" : ""}`}>
+                {currentWord}
+              </span>
+            </span>
+          </p>
+          <p className="text-sm font-light font-sans mb-5">
+            📍 San Francisco, CA
           </p>
         </div>
       </main>

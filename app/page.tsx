7ddd7fc1f99f1f22dbba54ecfd157f7eb3542656
anosharahim <anosharahim03@gmail.com>
2024-08-16 09:27:15 -0700

    add image content

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Image from "next/image";
 
 export default function Home() {
   const words = [
@@ -33,26 +34,51 @@ export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <main className="flex-grow container mx-auto py-6 flex items-center justify-center">
-        <div className="text-center">
-          {/* <p className="text-xl font-light font-sans mb-5">Hi, my name is</p> */}
-          <h1 className="text-4xl font-sans font-semi-light mb-8">
-            ANOSHA RAHIM
-          </h1>
+      <main className="pb-[30vh]">
+        <div className="flex flex-grow container mx-auto pt-[35vh] pb-[40vh] items-center justify-center">
+          <div className="text-center">
+            {/* <p className="text-xl font-light font-sans mb-5">Hi, my name is</p> */}
+            <h1 className="text-4xl font-sans font-semi-light mb-8">
+              ANOSHA RAHIM
+            </h1>
 
-          <p className="text-2xl font-light font-sans flex justify-center items-center mb-8">
-            <span className="word-container">
-              <span className={`word-change ${isChanging ? "changing" : ""}`}>
-                {currentWord}
+            <p className="text-2xl font-light font-sans flex justify-center items-center mb-8">
+              <span className="word-container">
+                <span className={`word-change ${isChanging ? "changing" : ""}`}>
+                  {currentWord}
+                </span>
               </span>
-            </span>
-          </p>
-          <p className="text-sm font-light font-sans mb-5">
-            📍 San Francisco, CA
-          </p>
+            </p>
+            <p className="text-sm font-light font-sans mb-5">
+              📍 San Francisco, CA
+            </p>
+          </div>
+        </div>
+        <div className="flex flex-col items-center justify-center min-h-screen">
+          <Image
+            src="/maxim-berg.jpg"
+            alt="Description of image"
+            width={200}
+            height={100}
+          />
+          <Image
+            src="/kier-in-sight.jpg"
+            alt="Description of image"
+            width={200}
+            height={100}
+          />
+
+          <Image
+            src="/geordanna-cordero.jpg"
+            alt="Description of image"
+            width={200}
+            height={100}
+          />
         </div>
       </main>
       <Footer />
     </div>
   );
+
+  //
 }

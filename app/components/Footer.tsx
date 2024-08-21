62ed3a1b9a461daab96b3e8518ba642bbcb0741e
anosharahim <anosharahim03@gmail.com>
2024-08-21 10:07:53 -0700

    add footer links

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -5,16 +5,28 @@ const Footer: React.FC = () => {
     <footer className="fixed bottom-0 left-0 right-0 py-4 px-4 ">
       <div className=" flex justify-center">
         <div className="flex justify-center space-x-4 mb-3 bg-gray-100 p-2 rounded-lg opacity-50">
-          <a href="#" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://github.com/anosharahim"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <FaGithub className="text-xl text-gray-800 hover:text-black transition duration-300" />
           </a>
-          <a href="#" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://linkedin.com/in/anosharahim"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <FaLinkedin className="text-xl text-gray-800 hover:text-black transition duration-300" />
           </a>
-          <a href="#" target="_blank" rel="noopener noreferrer">
+          {/* <a href="#" target="_blank" rel="noopener noreferrer">
             <FaInstagram className="text-xl text-gray-800 hover:text-black transition duration-300" />
-          </a>
-          <a href="#" target="_blank" rel="noopener noreferrer">
+          </a> */}
+          <a
+            href="https://twitter.com/anosha_rahim"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <FaTwitter className="text-xl text-gray-800 hover:text-black transition duration-300" />
           </a>
         </div>
